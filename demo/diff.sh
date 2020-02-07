afb319684905ec0f0b843b20076c972d99f82a47
Anthony Sottile <asottile@umich.edu>
2020-02-07 14:49:20 -0800

    check in a demo

---
+++ demo/diff.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -euo pipefail
+HERE="$(dirname "$(readlink -f "$0")")"
+exec python3 "${HERE}/../highlight_demo.py" \
+    highlight \
+    "${HERE}/themes/dark_vs.json" \
+    "${HERE}/languages/diff.tmLanguage.json" \
+    "${HERE}/files/patch.diff"
