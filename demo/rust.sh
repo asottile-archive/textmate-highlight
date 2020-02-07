c5c951dfa37332421de62a1df61381a6bd22efe4
Anthony Sottile <asottile@umich.edu>
2020-02-07 15:26:44 -0800

    Change demo to use dark_plus_vs instead

--- demo/rust.sh
+++ demo/rust.sh
@@ -3,6 +3,6 @@ set -euo pipefail
 HERE="$(dirname "$(readlink -f "$0")")"
 exec python3 "${HERE}/../highlight_demo.py" \
     highlight \
-    "${HERE}/themes/dark_vs.json" \
+    "${HERE}/themes/dark_plus_vs.json" \
     "${HERE}/languages/rust.tmLanguage.json" \
     "${HERE}/files/part2.rs"
