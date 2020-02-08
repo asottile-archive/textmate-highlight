c52ec58b7cdde404baa0e53cf1b26a940bbcc4e4
Anthony Sottile <asottile@umich.edu>
2020-02-08 15:45:28 -0800

    Add support for posessive quantifiers and \h and \H

--- tests/highlight_demo_test.py
+++ tests/highlight_demo_test.py
@@ -1,6 +1,9 @@
 import pytest
 
+from highlight_demo import _remove_comments
 from highlight_demo import _select
+from highlight_demo import _tokenize_re
+from highlight_demo import re_compile
 from highlight_demo import Selector
 
 
@@ -33,3 +36,123 @@ RULES = (
 )
 def test_select(scope, expected):
     assert _select(scope, RULES, 'trivial selector') == expected
+
+
+def test_re_compile_can_compile_a_regex():
+    pattern = re_compile('hello wo+rld')
+    assert pattern.match('hello woooooooooooooorld')
+
+
+def test_re_compile_possessive_quantifier_star():
+    pattern = re_compile('".*+"')
+    assert not pattern.match('"abc"')
+    assert not pattern.match('"abc"x')
+
+
+def test_re_compile_possessive_quantifier_plus():
+    pattern = re_compile('".++"')
+    assert not pattern.match('"abc"')
+    assert not pattern.match('"abc"x')
+
+
+def test_re_compile_multiple_possessive_quantifiers():
+    pattern = re_compile('"a*+" "b*+"')
+    assert pattern.match('"aaaaaa" "bbbbbbb"')
+
+
+def test_re_compile_not_a_possessive_quantifier():
+    pattern = re_compile(r'\*+.q')
+    assert pattern.match('*****q')
+
+
+def test_re_compile_character_class():
+    pattern = re_compile(r'"\s*+"')
+    assert pattern.groupindex
+    assert pattern.match('"    "')
+
+
+def test_re_compile_character_set():
+    pattern = re_compile('"[^"]++"')
+    assert pattern.groupindex
+    assert pattern.match('"hello world"')
+
+
+def test_re_compile_group():
+    pattern = re_compile('"(ab)++"')
+    assert pattern.groupindex
+    assert pattern.match('"ababab"')
+
+
+def test_re_compile_unicode_escape():
+    pattern = re_compile('"\u2603++"')
+    assert pattern.groupindex
+    assert pattern.match('"☃☃☃☃"')
+
+
+def test_re_compile_possessive_in_comment():
+    pattern = re_compile('(?# this is awesome, asottile++)hello')
+    assert not pattern.groupindex
+    assert pattern.match('hello')
+
+
+def test_re_compile_hex_escape():
+    pattern = re_compile(r'\h hello')
+    assert pattern.match('a hello')
+    assert pattern.match('A hello')
+    assert pattern.match('9 hello')
+    assert not pattern.match('q hello')
+
+
+def test_re_compile_hex_negation_escape():
+    pattern = re_compile(r'\H hello')
+    assert pattern.match('q hello')
+    assert not pattern.match('a hello')
+
+
+@pytest.mark.parametrize(
+    ('s', 'expected'),
+    (
+        ('', ''),
+        ('foo', 'foo'),
+        ('(?#foo)', ''),
+        (r'(?#foo \) bar)baz', 'baz'),
+        ('(?#foo\nbar)baz', 'baz'),
+        ('hello(?#wat)world', 'helloworld'),
+    ),
+)
+def test_remove_comments(s, expected):
+    assert _remove_comments(s) == expected
+
+
+@pytest.mark.parametrize(
+    ('s', 'expected'),
+    (
+        ('', {}),
+        ('foo', {}),
+        ('(foo)', {4: 0}),
+        ('[foo]', {4: 0}),
+        ('[fo)]', {4: 0}),
+        ('(foo[bar])', {8: 4, 9: 0}),
+        (r'\(foo\)', {1: 0, 6: 5}),
+        (r'\\(foo)', {1: 0, 6: 2}),
+        ('[]hi]', {4: 0}),
+        (r'\s', {1: 0}),
+        (r'\xa0aaa', {3: 0}),
+        (r'\xA0aaa', {3: 0}),
+        (r'\u2603aaa', {5: 0}),
+        (r'\u259Aaaa', {5: 0}),
+        (r'\U0001f643aaa', {9: 0}),
+        (r'\U0001F643aaa', {9: 0}),
+        (r'\0zzz', {1: 0}),
+        (r'\00zzz', {2: 0}),
+        (r'\01zzz', {2: 0}),
+        (r'\000zzz', {3: 0}),
+        (r'\001zzz', {3: 0}),
+        (r'\101zzz', {3: 0}),
+        (r'\1aaa', {1: 0}),
+        (r'\99aaa', {2: 0}),
+        (r'\199', {2: 0}),
+    ),
+)
+def test_tokenize_re(s, expected):
+    assert _tokenize_re(s) == expected
